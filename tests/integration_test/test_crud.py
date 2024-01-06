77cc7d6a8e86bffab5c898c67b50e79ed682ef56
KBSHari <93704459+KBSHari@users.noreply.github.com>
2024-01-06 19:03:04 +0530

    Delete tests/integration_test/test_crud.py
    test file deleted not required

--- tests/integration_test/test_crud.py
+++
@@ -1,8 +0,0 @@
-from src.constants.api_constants import APIConstants
-
-
-def test_urls():
-    print(APIConstants.base_url())
-    print(APIConstants.create_token())
-    print(APIConstants.create_booking())
-    print(APIConstants.put_patch_delete(2))
